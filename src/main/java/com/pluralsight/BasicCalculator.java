89d1da92b7a2c58ffa13ad0e0911cabfb30a2e28
takudzwa mucherengi <takudzwamucherengi@icloud.com>
2026-04-10 08:59:53 -0500

    6th Commit: Added switch statement and operators instead of A,S,M,D this improved my code efficiency and feel.

--- src/main/java/com/pluralsight/BasicCalculator.java
+++ src/main/java/com/pluralsight/BasicCalculator.java
@@ -6,76 +6,35 @@ public class BasicCalculator {
 
     public static void main(String[] args) {
 
-        //create a scanner
         Scanner theScanner = new Scanner(System.in);
 
-        //some header for the user
-        System.out.println("Enter two numbers: ");
 
-        //ask for the first number
+        // 1. First number
         System.out.print("First number: ");
         int firstNumber = theScanner.nextInt();
 
-
-
-
-        System.out.println("\n");
-
-
-        //Options Menu
-        /*
-        System.out.println("Possible Calculations: ");
-        System.out.println("(A)dd");
-        System.out.println("(S)ubtract");
-        System.out.println("(M)ultiply");
-        System.out.println("(D)ivide");
-        System.out.print("Please select an option: ");
+        // 2. Operator
+        System.out.print("Operator (+, -, *, /): ");
         String option = theScanner.next();
 
-         */
-        String option = theScanner.next();
-
-
-        switch (option) {
-            case "+" -> {
-
-                //add the numbers
-                int sum = firstNumber + secondNumber;
-                System.out.println("The sum is: " + sum);
-            }
-            case "-" -> {
-
-                //subtract the numbers
-                int difference = firstNumber - secondNumber;
-                System.out.println("The difference is: " + difference);
-            }
-            case "*" -> {
-
-                //multiply the numbers
-                int product = firstNumber * secondNumber;
-                System.out.println("The product is: " + product);
-            }
-            case "/" -> {
-
-                //divide the numbers
-                double quotient = (double) firstNumber / secondNumber;
-                System.out.println("The quotient is: " + quotient);
-            }
-        }
-
-
-        //ask for the second number
+        // 3. Second number
         System.out.print("Second number: ");
         int secondNumber = theScanner.nextInt();
 
-
-
-
-        //Answer verification
-        //System.out.println("Is the answer correct? (Yes/No): ");
-
-        //String answer = theScanner.next();
-        //System.out.println("You answered: " + answer);
+        // 4. Calculate
+        switch (option) {
+            case "+" -> System.out.println("The sum is: " + (firstNumber + secondNumber));
+            case "-" -> System.out.println("The difference is: " + (firstNumber - secondNumber));
+            case "*" -> System.out.println("The product is: " + (firstNumber * secondNumber));
+            case "/" -> {
+                if (secondNumber == 0) {
+                    System.out.println("Error: Cannot divide by zero.");
+                } else {
+                    System.out.println("The quotient is: " + (double) firstNumber / secondNumber);
+                }
+            }
+            default -> System.out.println("Invalid operator.");
+        }
 
     }
 }
